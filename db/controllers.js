1c083669ffb7700d17cedc2e1ca48b88af7f0c4f
Andrew Harris <andrewharris@andrews-mbp.home>
2022-07-07 10:29:16 +0100

    add get articles queries ex. err handling

--- db/controllers.js
+++ db/controllers.js
@@ -1,49 +1,62 @@
-const models= require('./models');
+const models = require("./models");
 
-exports.getTopics = (req,res) => {
-    models.fetchTopics().then((topics)=>{
-        res.send({topics});
+exports.getTopics = (req, res) => {
+  models.fetchTopics().then((topics) => {
+    res.send({ topics });
+  });
+};
+
+exports.getArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  models
+    .fetchArticle(article_id)
+    .then((article) => {
+      res.send({ article });
     })
-}
-
-exports.getArticle = (req,res,next) => {
-    const { article_id } = req.params;
-    models.fetchArticle(article_id).then((article)=>{
-        res.send({article});
-    }).catch(next)
-}
+    .catch(next);
+};
 
 exports.patchArticleById = (req, res, next) => {
-    const { article_id } = req.params;
-    const { inc_votes } = req.body;
-    models.updateArticleById(article_id, inc_votes).then((article)=>{
-      res.send({article})
-    }).catch(next)
-  };
-
-  exports.getUsers = (req,res) => {
-    models.fetchUsers().then((users)=>{
-        res.send({users});
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+  models
+    .updateArticleById(article_id, inc_votes)
+    .then((article) => {
+      res.send({ article });
     })
-}
+    .catch(next);
+};
 
-exports.getArticles = (req,res) => {
-    models.fetchArticles().then((articles)=>{
-        res.send({articles});
-    })
-}
+exports.getUsers = (req, res) => {
+  models.fetchUsers().then((users) => {
+    res.send({ users });
+  });
+};
+
+exports.getArticles = (req, res) => {
+  const { sort_by, order, filter } = req.query;
+  models.fetchArticles(sort_by, order, filter).then((articles) => {
+    res.send({ articles });
+  });
+};
 
 exports.getCommentsByArticleId = (req, res, next) => {
-    const { article_id } = req.params;
-    models.fetchCommentsById(article_id).then((comments)=>{
-        res.send({comments})
-    }).catch(next)
-  };
+  const { article_id } = req.params;
+  models
+    .fetchCommentsById(article_id)
+    .then((comments) => {
+      res.send({ comments });
+    })
+    .catch(next);
+};
 
-  exports.postComment = (req, res, next) => {
-    const {body, author} = req.body;
-    const {article_id} = req.params
-    models.insertComment(article_id, body, author).then((comment) => {
-       res.status(201).send({ comment })
-     }).catch(next)
-   };
+exports.postComment = (req, res, next) => {
+  const { body, author } = req.body;
+  const { article_id } = req.params;
+  models
+    .insertComment(article_id, body, author)
+    .then((comment) => {
+      res.status(201).send({ comment });
+    })
+    .catch(next);
+};
--- db/models.js
+++ db/models.js
@@ -1,118 +1,161 @@
-const db = require('./connection.js');
-const {articleData, commentData, topicData, userData}   = require('../db/data/test-data');
+const db = require("./connection.js");
+const {
+  articleData,
+  commentData,
+  topicData,
+  userData,
+} = require("../db/data/test-data");
 
 exports.fetchTopics = () => {
-    return db.query('SELECT * FROM topics;').then((topics)=>{
-        return topics.rows;
-    });
-}
-
-exports.fetchArticle = (id) => { 
-    if(isNaN(id)) {
-        return Promise.reject({
-          status: 400,
-          msg: `Incorrect data type`,
-        });
-      }
-      return db.query(`SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles
-      LEFT JOIN comments ON comments.article_id = articles.article_id
-      WHERE articles.article_id = $1
-      GROUP BY articles.article_id;`, [id]).then((result)=>{
-        if (!result.rows[0]) {
-            return Promise.reject({
-              status: 404,
-              msg: `No article found for article_id: ${id}`,
-            });
-          }
-        return result.rows[0]
-    })
+  return db.query("SELECT * FROM topics;").then((topics) => {
+    return topics.rows;
+  });
 };
 
-exports.updateArticleById = (id, vote = 0) => {
-    return db.query('UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;',[vote, id]).then((result) => {
-        if (!result.rows[0]) {
-            return Promise.reject({
-              status: 404,
-              msg: `No article found for article_id: ${id}`,
-            });
-    } else if (!vote) {
-        return Promise.reject({
-          status: 400,
-          msg: `Incorrect PATCH body input`,
-        });
-    }
-    return result.rows[0]
-});
-}
-
-exports.fetchUsers = () => {
-    return db.query('SELECT * FROM users;').then((users)=>{
-        return users.rows;
-    });
-}
-
-exports.fetchArticles = () => {
-    return db.query(`SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles
-    LEFT JOIN comments ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC;`).then((articles)=>{
-        return articles.rows;
-    });
-}
-
-exports.fetchCommentsById = (id) => {
-    if(isNaN(id)) {
-        return Promise.reject({
-          status: 400,
-          msg: `Incorrect data type`,
-        });
-      }
-    return db.query(`SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body FROM comments
-    LEFT JOIN articles ON comments.article_id = articles.article_id
-    WHERE comments.article_id = $1
-    GROUP BY comments.comment_id;`, [id]).then((comments)=>{
-        if (!comments.rows[0]) {
-            return db.query(`SELECT articles.article_id FROM articles`).then((article_id)=>{
-            if(article_id.rows.find(({ article_id }) => article_id == id)){
-            return ([
-              
-            ]);
-        } else{
-            return Promise.reject({
-                status: 404,
-                msg: `No article found for article_id: ${id}`,
-              });
-        }
-        })
-          }
-        return comments.rows;
-    });
-}
-
-exports.insertComment = (id, body, author) => {
-  if(isNaN(id)) {
+exports.fetchArticle = (id) => {
+  if (isNaN(id)) {
     return Promise.reject({
       status: 400,
       msg: `Incorrect data type`,
     });
-  } else if(!body || !author) {
+  }
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles
+      LEFT JOIN comments ON comments.article_id = articles.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;`,
+      [id]
+    )
+    .then((result) => {
+      if (!result.rows[0]) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${id}`,
+        });
+      }
+      return result.rows[0];
+    });
+};
+
+exports.updateArticleById = (id, vote = 0) => {
+  return db
+    .query(
+      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;",
+      [vote, id]
+    )
+    .then((result) => {
+      if (!result.rows[0]) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${id}`,
+        });
+      } else if (!vote) {
+        return Promise.reject({
+          status: 400,
+          msg: `Incorrect PATCH body input`,
+        });
+      }
+      return result.rows[0];
+    });
+};
+
+exports.fetchUsers = () => {
+  return db.query("SELECT * FROM users;").then((users) => {
+    return users.rows;
+  });
+};
+
+exports.fetchArticles = (sort_by = "created_at", order = "DESC", filter) => {
+  if (filter) {
+    return db
+      .query(
+        `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    WHERE articles.topic = '${String(filter)}'
+    GROUP BY articles.article_id
+    ORDER BY articles.${sort_by} ${order}`
+      )
+      .then((articles) => {
+        return articles.rows;
+      });
+  }
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    GROUP BY articles.article_id
+    ORDER BY articles.${sort_by} ${order}`
+    )
+    .then((articles) => {
+      return articles.rows;
+    });
+};
+
+exports.fetchCommentsById = (id) => {
+  if (isNaN(id)) {
+    return Promise.reject({
+      status: 400,
+      msg: `Incorrect data type`,
+    });
+  }
+  return db
+    .query(
+      `SELECT comments.comment_id, comments.votes, comments.created_at, comments.author, comments.body FROM comments
+    LEFT JOIN articles ON comments.article_id = articles.article_id
+    WHERE comments.article_id = $1
+    GROUP BY comments.comment_id;`,
+      [id]
+    )
+    .then((comments) => {
+      if (!comments.rows[0]) {
+        return db
+          .query(`SELECT articles.article_id FROM articles`)
+          .then((article_id) => {
+            if (article_id.rows.find(({ article_id }) => article_id == id)) {
+              return [];
+            } else {
+              return Promise.reject({
+                status: 404,
+                msg: `No article found for article_id: ${id}`,
+              });
+            }
+          });
+      }
+      return comments.rows;
+    });
+};
+
+exports.insertComment = (id, body, author) => {
+  if (isNaN(id)) {
+    return Promise.reject({
+      status: 400,
+      msg: `Incorrect data type`,
+    });
+  } else if (!body || !author) {
     return Promise.reject({
       status: 400,
       msg: `Post body incorrect`,
     });
-  } 
-return db.query(`SELECT articles.article_id FROM articles WHERE article_id = ${id}`).then((article_id)=>{
-  if(!article_id.rows.length){
-    return Promise.reject({
-      status: 404,
-      msg: `No article found for article_id: ${id}`,
-     })
   }
-  if(article_id.rows[0].article_id == id){
-    return db.query('INSERT INTO comments (body, author, article_id) VALUES ($1, $2, $3) RETURNING *;',[body, author, id]).then(({ rows }) => {
-      return rows[0]
-    })
-}
-  })
-}
-  
+  return db
+    .query(`SELECT articles.article_id FROM articles WHERE article_id = ${id}`)
+    .then((article_id) => {
+      if (!article_id.rows.length) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${id}`,
+        });
+      }
+      if (article_id.rows[0].article_id == id) {
+        return db
+          .query(
+            "INSERT INTO comments (body, author, article_id) VALUES ($1, $2, $3) RETURNING *;",
+            [body, author, id]
+          )
+          .then(({ rows }) => {
+            return rows[0];
+          });
+      }
+    });
+};
