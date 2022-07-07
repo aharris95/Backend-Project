b567b68a24316a33cdf36fa545cd79c8d6403531
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2022-07-07 17:04:09 +0100

    add heroku setup pt3

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 const config =
   ENV === 'production'
     ? {
@@ -15,7 +11,9 @@ const config =
       }
     : {};
 
-
+    require('dotenv').config({
+      path: `${__dirname}/../.env.${ENV}`,
+    });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
